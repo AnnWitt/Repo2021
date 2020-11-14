509aa17d03715f74afbfb458862566e4c7781a73
Anna Wittmann <annwitt@tlen.pl>
2020-11-14 23:28:08 +0100

    2020-11-14 Porzadkowanie katalogow

---
+++ Zbiorczy/src/main/java/BlokiTematyczne/Obiektowosc/Any.java
@@ -0,0 +1,17 @@
+package BlokiTematyczne.Obiektowosc;
+
+import BlokiTematyczne.Obiektowosc.Samochod;
+
+public class Any {
+    public static void main(String[] args) {
+        int i=10;
+        Samochod p1=new Samochod("Audi",2000,"czarny");
+        System.out.println(p1.getNazwa());
+        metoda1(p1);
+        System.out.println(p1.getNazwa());
+    }
+    public static void metoda1 (Samochod s) {
+        s.setNazwa("Mazda");
+        System.out.println(s.getNazwa());
+    }
+}
