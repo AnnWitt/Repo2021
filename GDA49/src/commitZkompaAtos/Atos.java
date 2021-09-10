294db01699aa6324c78bfc137630db3ca7048734
Anna Wittmann <annwitt@tlen.pl>
2021-09-10 17:02:25 +0200

    20210910 - testowanie repo z dwoch kompow

--- GDA49/src/commitZkompaAtos/Atos.java
+++ GDA49/src/commitZkompaAtos/Atos.java
@@ -5,5 +5,7 @@ public class Atos {
     public static void main(String[] args) {
         System.out.println("Dodane z Komputera służbowego");
         System.out.println("sprawdzam pusha");
+        System.out.println("push z kompa Ani prywatnego");
+
     }
 }
