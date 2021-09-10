289ccca3368d6ea1c60a9b312bf2dc56bcecd087
Anna Wittmann <anna.wittmann@atos.net>
2021-09-10 16:41:52 +0200

    20210910-testAtos

---
+++ GDA49/src/commitZkompaAtos/Atos.java
@@ -0,0 +1,9 @@
+package commitZkompaAtos;
+
+public class Atos {
+
+    public static void main(String[] args) {
+        System.out.println("Dodane z Komputera służbowego");
+        System.out.println("sprawdzam pusha");
+    }
+}
